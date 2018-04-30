e0f5db4bc11afa0b0869b93b60e8a07709f837db
Antoine du HAMEL <duhamelantoine1995@gmail.com>
2018-04-30 23:23:25 +0200

    Update demo

--- src/index.js
+++ src/index.js
@@ -1,9 +1,50 @@
 import yuml2svg from "yuml2svg";
 
+const LOADING_CLASS = "loading";
+const ERROR_CLASS = "error";
+
+let errorElement;
+
+const getErrorDialog = () => {
+  if (!errorElement) {
+    errorElement = document.createElement("div");
+    errorElement.className = ERROR_CLASS;
+    document.body.appendChild(errorElement);
+  } else {
+    errorElement.firstChild.remove();
+  }
+
+  return errorElement;
+};
+
+const setErrorDialog = error => {
+  getErrorDialog().appendChild(document.createTextNode(error.message));
+};
+
+const removeErrorDialog = () => {
+  if (errorElement) {
+    errorElement.remove();
+    errorElement = null;
+  }
+};
+
 document.addEventListener("DOMContentLoaded", () => {
-  document.getElementById("text-editor").addEventListener("input", event => {
-    yuml2svg(event.target.value, {}, { worker: "vizWorker.js" }).then(svg => {
-      document.getElementById("result").innerHTML = svg;
-    });
+  const textEditor = document.getElementById("text-editor");
+  const resultBlock = document.getElementById("result");
+
+  textEditor.addEventListener("input", event => {
+    resultBlock.classList.add(LOADING_CLASS);
+    yuml2svg(event.target.value, {}, { worker: "vizWorker.js" })
+      .then(svg => {
+        resultBlock.innerHTML = svg;
+        removeErrorDialog();
+      })
+      .catch(err => {
+        console.error(err);
+        setErrorDialog(err);
+      })
+      .finally(() => {
+        resultBlock.classList.remove(LOADING_CLASS);
+      });
   });
 });
--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,12 @@
+const path = require("path");
 const webpack = require("webpack");
 
 module.exports = {
+  output: {
+    // options related to how webpack emits results
+
+    path: path.resolve(__dirname, "docs"),
+  },
   node: false,
   externals: "viz.js/full.js.opaque",
   plugins: [
