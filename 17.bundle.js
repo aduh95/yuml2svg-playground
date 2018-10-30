76f11f7e487a049fb3ad9aafbd685546470b946c
Antoine du Hamel <antoine.duhamel@schneider-electric.com>
2018-10-30 12:27:45 +0100

    Updates

---
+++ 17.bundle.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[17],{20:function(s,t,i){"use strict";i.r(t);var e=i(19);t.default=class{constructor(){this._uidNb=0,this._uids={}}createUid(s,t){const i=Object(e.recordName)(s);return!this._uids.hasOwnProperty(i)&&(this._uids[i]=t?t("A"+this._uidNb++):"A"+this._uidNb++)}getUid(s){return this._uids[Object(e.recordName)(s)]}}}}]);
