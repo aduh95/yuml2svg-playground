4dc252c297c1c8fa1012c6fdc2abebac0f111c23
Antoine du HAMEL <duhamelantoine1995@gmail.com>
2019-06-16 11:10:31 -0400

    Upgrade deps

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,7 +1,7 @@
 const path = require("path");
 
 const webpack = require("webpack");
-const CleanWebpackPlugin = require("clean-webpack-plugin");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 const HtmlWebPackPlugin = require("html-webpack-plugin");
 
